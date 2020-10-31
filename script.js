2f3d4a0aeae1e297201b062dacf9a4045c0d3767
Alireza <alireza.az76@gmail.com>
2020-11-01 00:19:16 +0200

    put 0 for scores and hide the dice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+const dice = document.querySelector('.dice');
+dice.classList.add('hidden');
+const score0 = document.querySelector('#score--0');
+score0.textContent = '0';
+const score1 = document.querySelector('#score--1');
+score1.textContent = '0';
+
+const random = Math.floor(Math.random() * 6) + 1;
+document.querySelector('btn--roll').addEventListener('click', function () {
+  dice.classList.remove('hidden');
+});
