24b77d7507962bde2d69118a076c33d3183e732f
Alireza <alireza.az76@gmail.com>
2020-11-01 03:07:36 +0200

    Rolling button

--- script.js
+++ script.js
@@ -5,8 +5,24 @@ const score0 = document.querySelector('#score--0');
 score0.textContent = '0';
 const score1 = document.querySelector('#score--1');
 score1.textContent = '0';
+const btnRoll = document.querySelector('.btn--roll');
 
-const random = Math.floor(Math.random() * 6) + 1;
-document.querySelector('btn--roll').addEventListener('click', function () {
+let currentScore = 0;
+
+const current0 = document.querySelector('#current--0');
+const current1 = document.querySelector('#current--1');
+
+btnRoll.addEventListener('click', function () {
+  const randomDice = Math.trunc(Math.random() * 6) + 1;
+
   dice.classList.remove('hidden');
+
+  dice.src = `dice-${randomDice}.png`;
+
+  if (randomDice !== 1) {
+    console.log(currentScore);
+    currentScore = currentScore + randomDice;
+    current0.textContent = currentScore;
+  } else {
+  }
 });
