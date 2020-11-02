1c64b09f22a069ee8b5bddc82893f2abf987da81
Alireza <alireza.az76@gmail.com>
2020-11-02 03:22:30 +0200

    reseting the game

--- script.js
+++ script.js
@@ -7,19 +7,31 @@ const score1 = document.querySelector('#score--1');
 score1.textContent = '0';
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-
-let currentScore = 0;
-let activePlayer = 0;
-
+const btnNewGame = document.querySelector('.btn--new');
 const current0 = document.querySelector('#current--0');
 const current1 = document.querySelector('#current--1');
-
 const player0 = document.querySelector('.player--0');
 const player1 = document.querySelector('.player--1');
+let currentScore, activePlayer, score, playing;
 
-let score = [0, 0];
+const init = function () {
+  currentScore = 0;
+  activePlayer = 0;
+  score = [0, 0];
+  playing = true;
+  dice.classList.add('hidden');
+  score0.textContent = 0;
+  score1.textContent = 0;
+  current0.textContent = 0;
+  current1.textContent = 0;
+  playing = true;
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
+  player1.classList.add('player--active');
+  player0.classList.remove('player--active');
+};
 
-let playing = true;
+init();
 
 btnRoll.addEventListener('click', function () {
   if (playing) {
@@ -69,15 +81,14 @@ btnHold.addEventListener('click', function () {
           .classList.remove('player--active');
         dice.classList.add('hidden');
         playing = false;
-      } else {
-        activePlayer = 0;
-        player1.classList.toggle('player--active');
-        player0.classList.toggle('player--active');
-        currentScore = 0;
-        current1.textContent = currentScore;
       }
-    }
-    if (activePlayer === 0) {
+
+      activePlayer = 0;
+      player0.classList.toggle('player--active');
+      player1.classList.toggle('player--active');
+      currentScore = 0;
+      current1.textContent = currentScore;
+    } else if (activePlayer === 0) {
       score[1] = score[1] + Number(current0.textContent);
       score0.textContent = score[1];
       if (score[1] >= 10) {
@@ -89,13 +100,14 @@ btnHold.addEventListener('click', function () {
           .classList.remove('player--active');
         dice.classList.add('hidden');
         playing = false;
-      } else {
-        activePlayer = 1;
-        player0.classList.toggle('player--active');
-        player1.classList.toggle('player--active');
-        currentScore = 0;
-        current0.textContent = currentScore;
       }
+      activePlayer = 1;
+      player0.classList.toggle('player--active');
+      player1.classList.toggle('player--active');
+      currentScore = 0;
+      current0.textContent = currentScore;
     }
   }
 });
+
+btnNewGame.addEventListener('click', init);
